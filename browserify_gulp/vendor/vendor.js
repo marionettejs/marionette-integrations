876f8971fa0c1c987104b23bcea2b7bb4a36a5b2
denar90 <denysov.artem@gmail.com>
2016-05-20 13:52:33 +0300

    add browserify + gulp

---
+++ browserify_gulp/vendor/vendor.js
@@ -0,0 +1,7 @@
+import _ from  'underscore';
+import Backbone from 'backbone';
+import $ from 'jQuery';
+import Marionette from 'backbone.marionette';
+Backbone.$ = $;
+window.$ = $;
+export {_, $, Backbone, Marionette};
