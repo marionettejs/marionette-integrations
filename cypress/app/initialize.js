463faaaa56cbf00a70751ac90b682af69a07daad
denar90 <denysov.artem@gmail.com>
2016-05-22 15:38:36 +0300

    add cypress

---
+++ cypress/app/initialize.js
@@ -0,0 +1,7 @@
+import {$} from '../vendor/vendor';
+import App from './components/App';
+
+document.addEventListener('DOMContentLoaded', () => {
+  var app = new App();
+  app.start();
+});
---
+++ cypress/vendor/vendor.js
@@ -0,0 +1,8 @@
+import _ from  'underscore';
+import Backbone from 'backbone';
+import $ from 'jQuery';
+import Marionette from 'backbone.marionette';
+Backbone.$ = $;
+window = window || {};
+window.$ = $;
+export {_, $, Backbone, Marionette};
