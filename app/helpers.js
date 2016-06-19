bab0e285af11eef89f6bb7c50e377bf2d0e4cfb1
denar90 <denysov.artem@gmail.com>
2016-06-19 09:43:55 +0300

    add yo

---
+++ app/helpers.js
@@ -0,0 +1,34 @@
+module.exports =  {
+
+  getOption: function (boilerplates, options) {
+    var option;
+
+    for (var i in boilerplates) {
+      var type = boilerplates[i];
+
+      if (options[type]) {
+        option = type;
+        break;
+      }
+    }
+
+    if (option) {
+      return option;
+    } else {
+      throw new Error('Wrong boilerplate type was set');
+    }
+  },
+
+  getSourcePath: function (sourceRoot, boilerplateType) {
+    return sourceRoot.replace(/app\/templates/gi, boilerplateType);
+  },
+
+  generateApp: function (boilerplates) {
+    var option = this.getOption(boilerplates, this.options);
+    var sourceRoot = this.sourceRoot();
+    var source = this.getSourcePath(sourceRoot, option);
+    var destination = this.destinationRoot(option);
+
+    this.fs.copy(source, destination);
+  }
+};
--- readme.md
+++ readme.md
@@ -15,3 +15,22 @@ Example marionette apps using multiple dependency tools.
  * [Karma + Jasmine](/jasmine)
  * [Cypress](/cypress)
  * [CasperJS](/casperjs)
+
+
+## Want to try examples fast?
+
+ * npm install -g yo
+ * npm install -g generator-marionette-integrations
+
+ ### Commands
+
+ * yo marionette-integrations:app --browserify
+ * yo marionette-integrations:app --browserify-gulp
+ * yo marionette-integrations:app --browserify-grunt
+ * yo marionette-integrations:app --brunch
+ * yo marionette-integrations:app --casperjs
+ * yo marionette-integrations:app --cypress
+ * yo marionette-integrations:app --jasmine
+ * yo marionette-integrations:app --mocha
+
+Each command will generate for you related application example.
